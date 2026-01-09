52c7a97297762971d373a034af6e2016dfe15f19
karmannl637-code <karmannl637@gmail.com>
2026-01-08 19:40:39 -0500

    Learn Varibles

--- Data_type.py
+++ Data_type.py
@@ -1,4 +1,20 @@
 a = 5 
 b = 10
 print(a+b)
-print(a-b)
+print(a-b) 
+
+my_number = 78 
+print(my_number)
+print(type(my_number))
+
+passed = True
+print(passed)
+print(type(passed))
+
+name = "Karmann"
+print(name)
+print(type(name))
+
+temperature = 98.7
+print(temperature)
+print(type(temperature))
---
+++ Varabiles.py
@@ -0,0 +1,10 @@
+a, b = 50, 10
+
+print ("a + b = ", a+b)
+print("a-b = ", a-b)
+print("a * b =", a*b)
+print("a/b =", a/b)
+print("b/a =", b/a)
+
+print(f"a+b = {a+b}")
+print(f"a-b = {a-b}")
