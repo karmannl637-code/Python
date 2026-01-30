d702ee3f3261311f6ca4de216c6d057a436b6c80
karmannl637-code <karmannl637@gmail.com>
2026-01-29 19:42:16 -0500

    today I learned operators

---
+++ Operators.py
@@ -0,0 +1,34 @@
+x = 2
+y = 4
+
+print(x**y)
+print(x/y)
+print(x//y)
+
+a = 100
+b = 5
+
+
+print(a**b) # This means 100 to the power of 5
+print(a/b) # This one means  100 is divided by 5
+print(a//b) # This means rounding down to nearest interger
+
+print(a%b) # This is dividing 100 by 5 which the reminder is 0 
+print(b%a) # This is dividing 5 by 100 and the reminder is 5 
+
+c = 6 
+d = 5
+
+print(c == d)
+print(c != d)
+print((c!=d) == (d!=c))
+
+e = 10
+f = 5
+
+print(f > e)
+print(e > f)
+print((e > f) == (f < e))
+
+
+
