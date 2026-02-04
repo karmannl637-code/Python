28df78f30f532feebadf9e3df8e09f64cb78ef36
karmannl637-code <karmannl637@gmail.com>
2026-02-03 21:28:00 -0500

    Be Happy

---
+++ Conditional.py
@@ -0,0 +1,28 @@
+# age = int(input("Enter your age ")) 
+# if age >= 18:
+#     print("you can vote")
+# else:
+#     print(f"Come back after {18 - age} years ")
+
+# license = (input("Do you have a license yes, or no"))
+# if license == " yes":
+#     print ("You are allowed to drive")
+# else:
+#     print(f"You are not allowed to drive yet, come back when you have a lincense ")
+
+# age = int(input("Enter your age ")) 
+# if age >= 18:
+#     print("You can vote")
+# elif age >= 11:
+#     print("You can learn how voting works ")
+# elif age <= 7:
+#     print("You need to eat healthy food to grow faster")
+# else:
+#     print(f"Come back after {18 - age} years to vote")
+
+license = (input("Do you have a license yes, or no"))
+if license == " yes":
+    print ("You are allowed to drive")
+else:
+    print(f"You are not allowed to drive yet, come back when you have a lincense ")
+    
