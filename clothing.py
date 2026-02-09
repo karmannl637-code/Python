30110d166347dde484d73f81a2cff9b025780311
karmannl637-code <karmannl637@gmail.com>
2026-02-08 22:00:26 -0500

    I AM DONE

---
+++ clothing.py
@@ -0,0 +1,10 @@
+#To cheack clothing based on temp
+
+temperature = int(input("Enter the temperature in °C: "))
+
+if temperature >= 25:
+    print("You can wear light and soft clothes because its warm.")
+elif temperature >= 15 and temperature < 25:
+    print("You can wear light clothes, but carry a light jacket because it might be chilly.")
+else:
+    print("It is cold. Wear a jacket or pullover.")
