4da66ee232f375085a8bc519306b133905ab24ca
karmannl637-code <karmannl637@gmail.com>
2026-03-03 21:32:00 -0500

    HAPPY CHOTI HOLI

---
+++ If elif else/Avg.py
@@ -0,0 +1,5 @@
+a = 4
+b = 2
+c = 2
+avg = (a+b+c)/3
+print(avg)
---
+++ If elif else/Divisible.py
@@ -0,0 +1,9 @@
+num_1 = int(input("Enter your 1st number")) 
+num_2 = int(input("Enter your 2nd number"))
+
+if num_1% num_2 == 0 :
+    print("Number 1 is divisible by Number 2")
+
+else:
+    print("Number 1 isn't divisible by Number 2")
+    
