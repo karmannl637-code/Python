66cafabc1441b327a2e3daa6df7e8123294c494a
karmannl637-code <karmannl637@gmail.com>
2026-03-24 21:29:52 -0400

    YAY

---
+++ Loops/For_Loop.py
@@ -0,0 +1,24 @@
+x = " Banana, Pineapple, Mango, Strawberry, Blueberry,"
+print(x)
+print(x[0])
+print(x[1])
+print(x[2])
+print(x[3])
+print(x[4])
+print(""*5)
+
+
+
+
+#for i in range(6):
+   # print(x[i],end = "" )
+
+#for i in range(6):
+   # print(x[i],end = "" )
+
+for i in range(7):
+     print(x[i],end = "" )
+print()
+
+for i in range(-10, -1 ):
+    print(x[i],end = "" )
